99b4e30425cd9ca7a7d640f2464fd027b476834d
kathiou <kathiou@kathiou-pc.kalihome>
2023-01-25 15:03:33 +0100

    new commands

--- enum.sh
+++ enum.sh
@@ -9,11 +9,21 @@ mkdir "$dir_name"
 # Prompt user for target IP
 read -p "Enter the target IP address: " target_ip
 
+# Copy the IP address to a file named info.txt
+echo "IP :$target_ip" > "$dir_name/info.txt"
+
 # Ping the target IP 4 times
 ping -c 4 "$target_ip"
 
-# Create the "nmap" directory
+# Create the "nmap" and "tests" directorys
 mkdir "$dir_name/nmap"
+mkdir "$dir_name/tests"
 
 # Run nmap scan and save the results to a file
 nmap -sC -sV -oN "$dir_name/nmap/results.txt" -p- --min-rate=1000 -v "$target_ip"
+
+# Open the info.txt file with mousepad text editor
+mousepad "$dir_name/info.txt" &
+
+# Change directory to tests
+cd "$dir_name/tests"
